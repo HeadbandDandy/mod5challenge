9e5e1920f4c9476e07b52799a9a1581b503f3443
Darryl Johnson <dandycodes@gmail.com>
2022-07-14 12:33:31 -0400

    added moment.js file app functions

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
-var todayDate = moment().format('dddd, MMMM Do YYYY');
-$('#currentDay').html(todayDate);
 
 
-$(document).ready(function (){  //page loads
-    $('.saveBtn').on('click', function(){
-        console.log(this);      //gets nearby values
-        var text = $(this).siblings('description').val(); //takes change from sibling html description
-        var time = $(this).parent().attr('id'); // taken the change from the parent id attribute
+$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-        localStorage.setItem(time, text)
+        //set items in local storage.
+        localStorage.setItem(time, text);
     })
-})
 
 
-$('#hour1.description').val(localStorage.getItem('hour1'));
-$('#hour2.description').val(localStorage.getItem('hour2'));
-$('#hour3.description').val(localStorage.getItem('hour3'));
-$('#hour4.description').val(localStorage.getItem('hour4'));
-$('#hour5.description').val(localStorage.getItem('hour5'));
-$('#hour6.description').val(localStorage.getItem('hour6'));
-$('#hour7.description').val(localStorage.getItem('hour7'));
-$('#hour8.description').val(localStorage.getItem('hour8'));
-$('#hour9.description').val(localStorage.getItem('hour9'));
-$('#hour10.description').val(localStorage.getItem('hour10'));
-$('#hour11.description').val(localStorage.getItem('hour11'));
-$('#hour12.description').val(localStorage.getItem('hour12'));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
 //loads saved Data to localStorage
+//when doing above the "hours" must match how the time fits on a military clock based on 24 hours
 
 
-function hourTracker () {
-    var currentHour = moment().hour();
-// gets the current hour
+function hourTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); // use of moment.js
 
-    $('.time-block').each(function(){
-        var blockHour = parseInt($(this).attr('id').split('hour')[1]);
-        console.log(blockHour, currentHour)
-        // loops the time blocks
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
 
+        //check if we've moved past this time, click into css/html given classes of past, present, or future
+        //below changes the colors of each section based on relevance to time. 
         if (blockHour < currentHour) {
-            $(this).addClass('past');
-            $(this).removeClass('future')
-            $(this).removeClass('present')
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
         }
         else if (blockHour === currentHour) {
-            $(this).removeClass('past')
-            $(this).addClass('present')
-            $(this).removeClass('future')
-        } 
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
         else {
-            $(this).removeClass('present')
-            $(this).removeclass('past')
-            $(this).addClass('future')
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
         }
     })
-    hourTracker();
-
 }
+hourTracker(); //re-run function
+})
